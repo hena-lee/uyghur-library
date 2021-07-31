f08760506d4e52da62d2622d756b5c493625626e
Hena <lee.henajane@gmail.com>
2021-07-31 02:58:44 -0500

    Adds delete book feature

--- uyghur_library/uyghur_library/urls.py
+++ uyghur_library/uyghur_library/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('upload/', views.upload, name='upload'),
     path('books/', views.book_list, name='book_list'),
     path('books/upload/', views.upload_book, name='upload_book'),
+    path('books/<int:pk>/', views.delete_book, name='delete_book'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
