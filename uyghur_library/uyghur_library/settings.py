34420199117e2bf29c00f8d0e0d5ab90afcd9a82
Hena <lee.henajane@gmail.com>
2021-07-31 02:36:33 -0500

    Added django-heroku packages

--- uyghur_library/uyghur_library/settings.py
+++ uyghur_library/uyghur_library/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,3 +137,5 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'library-home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
